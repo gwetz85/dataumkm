8bd5b2ee7c97004e60a9c8e8dbc70a7a607c7277
Agus Suriyadi <agus.sahabathanura@gmail.com>
2026-02-22 05:26:56 +0000

    hapus tombpl tambah data baru di database , tambah data hanya bisa di la

--- src/app/database/page.tsx
+++ src/app/database/page.tsx
@@ -3,9 +3,6 @@
 import * as React from 'react';
 import { EntrepreneurDataTable } from '@/components/entrepreneur-data-table';
 import { useEntrepreneur } from '@/context/EntrepreneurContext';
-import { Button } from '@/components/ui/button';
-import { PlusCircle } from 'lucide-react';
-import Link from 'next/link';
 import { Skeleton } from '@/components/ui/skeleton';
 
 
@@ -16,12 +13,6 @@ export default function DatabasePage() {
     <>
         <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4">
             <h1 className="text-3xl font-headline font-bold">Database UMKM</h1>
-            <Button asChild className="shadow-md w-full md:w-auto">
-                <Link href="/input-data">
-                    <PlusCircle className="mr-2" />
-                    Tambah Data Baru
-                </Link>
-            </Button>
         </div>
         {loading ? (
              <div className="space-y-4">

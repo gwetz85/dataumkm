e4012a1d122a78bd6e81bc8c0fd79b66a65b30db
Agus Suriyadi <agus.sahabathanura@gmail.com>
2026-02-23 06:17:44 +0000

    icon untuk user profile

--- src/app/profil/page.tsx
+++ src/app/profil/page.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import { ProfileForm } from '@/components/profile-form';
 import { useAuth, type UserProfile } from '@/context/AuthContext';
 import { Skeleton } from '@/components/ui/skeleton';
+import { User } from 'lucide-react';
 
 export default function ProfilePage() {
     const { user, updateUserProfile, loading } = useAuth();
@@ -23,7 +24,10 @@ export default function ProfilePage() {
 
     return (
         <div className="space-y-8">
-            <h1 className="text-3xl font-headline font-bold">Profil Pengguna</h1>
+            <h1 className="text-3xl font-headline font-bold flex items-center gap-3">
+                <User className="w-8 h-8" />
+                Profil Pengguna
+            </h1>
             <ProfileForm onFormSubmit={handleFormSubmit} initialData={user?.data} />
         </div>
     );
